cbb9ebff8584611f123a56a1b1a524bcf247b012
MEC369 <marceubert886@gmail.com>
2025-04-06 01:32:37 +0800

    initial update for api request in fetching a user

--- src/pages/chats/index.tsx
+++ src/pages/chats/index.tsx
@@ -1,21 +1,67 @@
 import React from "react";
 import Image from "next/image";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-// export async function getServerSideProps() {
-//   try {
-//     const req = await fetch("/api/validator", {
-//       headers: {
-//         "Content-Type": "application/json",
+// export const getServerSideProps = async ({ req, res }) => {
+//   try {    
+//     const token = req.headers.cookie;
+//     const decodedToken = JSON.parse(decodeURIComponent(token.split('=')[1]));
+    
+//     const request = await fetch(`/api/users/user`);
+//     const response = await request.json();
+//     console.log('request', response);
+    
+
+//     return {
+//       props: {
+//         test: decodedToken.id,
 //       },
-//       method: "POST",
-//       body: JSON.stringify({ token: decodeURIComponent(token.value) }),
-//     });
+//     };
 //   } catch (error) {
-//     console.error(error);
+//     return {
+//       props: {
+//         test: 'error',
+//       },
+//     };
 //   }
-// }
+// };
 
-export default function Chats() {
+interface ssrProps {
+  sample: string
+}
+
+export const getServerSideProps = (async ({req}) => {
+  try {
+    const token = req.headers.cookie;
+    const decodedToken = JSON.parse(decodeURIComponent(token.split('=')[1]));
+    const protocol = req.headers['x-forwarded-proto'];
+    const origin: string = `${protocol}://${req.headers.host}`
+    const request = await fetch(`${origin}/api/users/${decodedToken.id}`);
+    const response = await request.json();
+    console.log('response', response);
+    
+
+    return {
+      props: {
+        test: {
+          sample: decodedToken.id
+        } 
+      }
+    }
+  } catch (error) {
+    return {
+      props: {
+        test: {
+          sample: `${error}`
+        } 
+      }
+    }
+  }
+}) satisfies GetServerSideProps<{test: ssrProps}>
+
+export default function Chats({ test }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log("SSR test", test);
+
   return (
     <div className="w-full h-full flex items-center justify-center flex-col gap-3">
       <div className="relative w-2/3 h-2/3">
