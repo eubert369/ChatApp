8ee865dd0ce34ef677ca62d7d5053f8c77a0ef04
MEC369 <marceubert886@gmail.com>
2025-05-18 21:56:32 +0800

    now able to create a message

--- src/pages/api/messages/chat.ts
+++ src/pages/api/messages/chat.ts
@@ -1,12 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/components/firebase/Config";
+import { collection, addDoc } from "firebase/firestore";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const stringToken = req.cookies.token;
-    // const { convoId, recipientId, message } = req.body;
+  const { convoId, recipientId, message } = req.body;
   if (req.method == "POST") {
     if (stringToken) {
       const token = JSON.parse(decodeURIComponent(atob(stringToken)));
-      res.status(200).json({ senderId: token.id, reqBody: req.body });
+
+      try {
+        await addDoc(collection(db, "messages"), {
+          convoId: convoId,
+          messageContent: message,
+          recipientId: recipientId,
+          senderId: token.id,
+        });
+
+        res.status(200).json({ message: "Message send successfully" });
+      } catch (error) {
+        res.status(500).json({ message: "Cannot send message", err: error });
+      }
     }
   }
 }
